7a80449fd5fd9979c267f3ca53c626a9645418b5
Ayush Kesarwani <kaayush112@gmail.com>
2022-04-11 12:57:12 +0530

    Junit Testing for update,delete,getEmployeesByLastName, and By email added

--- spring-boot-rest-datajpa/src/test/java/com/cg/springbootrestdatajpa/EmployeeControllerIntegrationTest.java
+++ spring-boot-rest-datajpa/src/test/java/com/cg/springbootrestdatajpa/EmployeeControllerIntegrationTest.java
@@ -1,18 +1,20 @@
+// Working with the real database.
+
 package com.cg.springbootrestdatajpa;
 
-import org.junit.jupiter.api.Test;
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
-
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpHeaders;
 
 import com.cg.springbootrestdatajpa.model.Employee;
 
@@ -54,7 +56,67 @@ public class EmployeeControllerIntegrationTest {
 	@Test
 	public void testGetEmployeeById()
 	{
-		Employee employee = restTemplate.getForObject(getRootUrl()+"/api/employees/byid/17", Employee.class);
+		long id=12;
+		Employee employee = restTemplate.getForObject(getRootUrl()+"/api/employees/byid/"+id, Employee.class);
+		//assertEquals(id, employee.getEmpId());
 		assertNotNull(employee);
 	}
+	
+	@Test
+	public void testUpdateEmployee()
+	{
+		long id = 15;
+		//Load Object
+		Employee employee = restTemplate.getForObject(getRootUrl()+"/api/employees/byid/"+id, Employee.class);
+		employee.setFirstName("Update Testing");
+		employee.setLastName("Update L-name");
+		employee.setEmailId("Update@gmail.com");
+		
+		restTemplate.put(getRootUrl()+"/api/employees/update/"+id, employee);
+		
+		Employee updatedEmployee = restTemplate.getForObject(getRootUrl()+"/api/employees/byid/"+id, Employee.class);
+		
+		assertNotNull(updatedEmployee);
+		assertEquals(employee.getFirstName(),updatedEmployee.getFirstName());
+	}
+	
+	@Test
+	public void testGetEmployeesByLastName()
+	{
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<String> entity = new HttpEntity<String>(null,headers);
+		ResponseEntity<String> response = restTemplate.exchange(getRootUrl()+"/api/employees/bylastname/kumar",HttpMethod.GET,entity,String.class);
+		assertNotNull(response.getBody());
+	}
+	
+	@Test
+	public void testDeleteEmployee()
+	{
+		long id = 20;
+		Employee employee = restTemplate.getForObject(getRootUrl()+"/api/employees/byid/"+id, Employee.class);
+		//System.out.println(employee);
+		assertEquals(id, employee.getEmpId());
+		//assertNotNull(employee);
+		
+		restTemplate.delete(getRootUrl()+"/api/employees/delete/id/"+id,employee);
+		
+		// checking for the data is deleted or not
+		Employee deletedEmployee = restTemplate.getForObject(getRootUrl()+"/api/employees/byid/"+id, Employee.class);
+		assertNotEquals(id, deletedEmployee.getEmpId());
+		
+		
+		/*}catch(final HttpClientErrorException e)
+		{
+			System.out.println(e.getMessage());
+			assertEquals(e.getStatusCode(), HttpStatus.NOT_FOUND);
+		}*/
+	}
+	
+	@Test
+	public void testGetEmployeeByEmailId()
+	{
+		Employee employee = restTemplate.getForObject(getRootUrl()+"/employees/byemail/Ms@gmail.com", Employee.class);
+		assertNotNull(employee);
+	}
+	
 }
