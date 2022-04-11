b2d8239d65c327783037f421539e22f7991f99d7
Ayush Kesarwani <kaayush112@gmail.com>
2022-04-11 10:55:00 +0530

    Junit Testing for getAllEmployess and GetEmployeeById added

--- spring-boot-rest-datajpa/src/test/java/com/cg/springbootrestdatajpa/EmployeeControllerIntegrationTest.java
+++ spring-boot-rest-datajpa/src/test/java/com/cg/springbootrestdatajpa/EmployeeControllerIntegrationTest.java
@@ -3,11 +3,16 @@ package com.cg.springbootrestdatajpa;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertNotNull;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpHeaders;
 
 import com.cg.springbootrestdatajpa.model.Employee;
 
@@ -29,11 +34,27 @@ public class EmployeeControllerIntegrationTest {
 	public void testCreateEmployee()
 	{
 		Employee employee = new Employee();
-		employee.setFirstName("Testing F-name");
-		employee.setLastName("Testing L-name");
-		employee.setEmailId("Test@gmail.com");
+		employee.setFirstName("Testing2 F-name");
+		employee.setLastName("Testing2 L-name");
+		employee.setEmailId("Test2@gmail.com");
 		ResponseEntity<Employee> postResponse = restTemplate.postForEntity(getRootUrl() + "/api/employees/new", employee, Employee.class);
-		assertNotNull(postResponse);
+		//assertNotNull(postResponse);
 		assertNotNull(postResponse.getBody());
+	}
+	
+	@Test
+	public void testGetAllEmployees()
+	{
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<String> entity = new HttpEntity<String>(null,headers);
+		ResponseEntity<String> response = restTemplate.exchange(getRootUrl()+"/api/employees/all",HttpMethod.GET,entity,String.class);
+		assertNotNull(response.getBody());
+	}
+	
+	@Test
+	public void testGetEmployeeById()
+	{
+		Employee employee = restTemplate.getForObject(getRootUrl()+"/api/employees/byid/17", Employee.class);
+		assertNotNull(employee);
 	}
 }
